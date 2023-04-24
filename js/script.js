958c627261c6dbec4d4a687685f633d37a7eba92
KevinGibram <kevinrincon0911@gmail.com>
2023-04-24 00:53:56 -0500

    agregando funciones al boton principal del formulario

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-//querySelector
-const heading = document.querySelector(' .header__texto h2'); // retorna de 0 a 1max elemento
-heading.textContent = "nuevo texto"
+// //querySelector
+// const heading = document.querySelector(' .header__texto h2'); // retorna de 0 a 1max elemento
+// heading.textContent = "nuevo texto"
 
-//querySelectorAll
-const enlaces = document.querySelectorAll(' .navegacion a');
-enlaces[5].textContent = " number 5"
-
-
-//agregar eventos
-
-console.log (1);
-
-//cuando carge el html, css, js
-window.addEventListener("load", function(){
-    console.log (4);
-});
-
-// cuando cargue el html pero no espera ni css ni js
-document.addEventListener("DOMContentLoaded", function(){
-    console.log (3);
-});
-
-console.log (2);
+// //querySelectorAll
+// const enlaces = document.querySelectorAll(' .navegacion a');
+// enlaces[5].textContent = " number 5"
 
 
+// //agregar eventos
+
+// console.log (1);
+
+// //cuando carge el html, css, js
+// window.addEventListener("load", function(){
+//     console.log (4);
+// });
+
+// // cuando cargue el html pero no espera ni css ni js
+// document.addEventListener("DOMContentLoaded", function(){
+//     console.log (3);
+// });
+
+// console.log (2);
+
+const btclick = document.querySelector('boton--principal'); //selec un elemento del html
+
+document.addEventListener('click', function(evento){ //estamos creando un evento (e, event, evento evt)
+    console.log(evento);
+    evento.preventDefault(); //quita la redireccion
+    console.log('cargando evento');
+})

448fa1d3fe129094ea846dc05de9ea3843e74a78
KevinGibram <kevinrincon0911@gmail.com>
2023-04-23 22:53:55 -0500

    agregando eventos al js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ heading.textContent = "nuevo texto"
 //querySelectorAll
 const enlaces = document.querySelectorAll(' .navegacion a');
 enlaces[5].textContent = " number 5"
+
+
+//agregar eventos
+
+console.log (1);
+
+//cuando carge el html, css, js
+window.addEventListener("load", function(){
+    console.log (4);
+});
+
+// cuando cargue el html pero no espera ni css ni js
+document.addEventListener("DOMContentLoaded", function(){
+    console.log (3);
+});
+
+console.log (2);
+
+

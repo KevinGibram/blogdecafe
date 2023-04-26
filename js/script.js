b2e372774c942fd87f4f03432c572befa587f4ff
KevinGibram <kevinrincon0911@gmail.com>
2023-04-26 00:03:49 -0500

    validando formulario

--- js/script.js
+++ js/script.js
@@ -23,25 +23,78 @@
 
 // console.log (2);
 
-const btprincipal = document.querySelector('#btprincipal'); //selec un elemento del html
+// const btprincipal = document.querySelector('#btprincipal'); //selec un elemento del html
 
-btprincipal.addEventListener('click', function(evento){ //estamos creando un evento (e, event, evento evt)
-    evento.preventDefault(); //quita la redireccion
-    console.log('cargando evento');
-})
-
-
+// btprincipal.addEventListener('click', function(evento){ //estamos creando un evento (e, event, evento evt)
+//     evento.preventDefault(); //quita la redireccion
+//     console.log('cargando evento');
+// })
+ 
 
 //tomar valores de un label
+const datos = {
+    nombre: '' ,
+    email: '',
+    textarea: ''
+}
 
  const nombre = document.querySelector('#nombre');
  const email = document.querySelector('#email');
  const textarea = document.querySelector('#textarea');
+const formulario = document.querySelector('.formulario');
 
- nombre.addEventListener('input', leerTexto)
- email.addEventListener('input', leerTexto)
- textarea.addEventListener('input', leerTexto)
+ nombre.addEventListener('input', leerTexto);
+ email.addEventListener('input', leerTexto);
+ textarea.addEventListener('input', leerTexto);
 
+//Creando eventos del submit
+
+formulario.addEventListener('submit', function(e){
+   e.preventDefault()
+
+   //Validar formulario
+    const {nombre, email, textarea} = datos;
+    if( nombre === '' || email === '' || textarea === '' ){
+        mostrarError('Todos los datos son obligatorios');
+
+        return; //Corta la ejecucion del codigo
+    }
+    
+   //Enviar formulario
+   mostrarEnvio('Enviando Formulario');
+})
+
+//Tomando datos del imput
  function leerTexto(e) {
-    console.log(e.target.value)
+    // console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value;
+    // console.log(datos);
+
+}
+
+//Muestra mensaje en pantalla
+
+function mostrarError(mensaje){
+    const error = document.createElement('P') //creando el parrafo 
+    error.textContent = mensaje; // agregandole de contenido el mensaje
+    error.classList.add('error'); //agregando una clase 
+
+    formulario.appendChild( error ); //mostrando en pantalla
+
+    setTimeout(function() {
+        error.remove(); // eliminando el párrafo después de 3 segundos
+      }, 3000);
+}
+
+function mostrarEnvio(mensaje){
+    const envio = document.createElement('P') //creando el parrafo 
+    envio.textContent = mensaje; // agregandole de contenido el mensaje
+    envio.classList.add('envio'); //agregando una clase 
+
+    formulario.appendChild( envio ); //mostrando en pantalla
+
+    setTimeout(function() {
+        envio.remove(); // eliminando el párrafo después de 3 segundos
+      }, 3000);
 }
